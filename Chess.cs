74de349eb7b04e6845f2db771078d60bd375e652
JugHuter <71510739+JugHuter@users.noreply.github.com>
2020-10-30 20:48:08 +0500

    Update Chess.cs

--- Chess.cs
+++ Chess.cs
@@ -4,6 +4,14 @@ namespace Chess
 {
     class Program
     {
+         public static bool Suitable(string a)
+        {
+            if (a[0] >= 'A' && a[0] <= 'H' && a[1] >= '1' && a[1] <= '8' && a.Length == 2)
+                return true;
+            else
+                return false;
+
+        }
         static void Main(string[] args)
         {
             char[,] board = new char[8, 8];
